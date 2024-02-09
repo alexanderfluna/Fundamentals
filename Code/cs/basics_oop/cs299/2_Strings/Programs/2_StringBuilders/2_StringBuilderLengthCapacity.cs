b72b6c629c73f0c8f35444917db2c08479cf0a65
Alexander Luna <59184968+alexanderfluna@users.noreply.github.com>
2024-02-09 11:35:44 -0500

    Update 2_StringBuilderLengthCapacity.cs

--- Code/cs/basics_oop/cs299/2_Strings/Programs/2_StringBuilders/2_StringBuilderLengthCapacity.cs
+++ Code/cs/basics_oop/cs299/2_Strings/Programs/2_StringBuilders/2_StringBuilderLengthCapacity.cs
@@ -4,36 +4,40 @@ using System.Text;
 namespace ConsoleApplication1
 {
     /// <summary
-	/// This program demonstrates the length and capacity methods 
-	/// of the StringBuilder class.
-	
-	public class StringBuilderLengthCapacity 
-	{
-		public static void Main()
-		{
-			Console.WriteLine("Stringresultfer length and capacity Methods\n");
-			
-			StringBuilder result =
-				new StringBuilder( "Hello" );
+    /// This program demonstrates the length and capacity methods 
+    /// of the StringBuilder class.
 
-			Console.WriteLine("result = {0}, Length = {1}, Capacity = {2}, MaxCapacity = {3}",
-				             result, result.Length, result.Capacity, result.MaxCapacity);
-			
-			result.Append(", how are you?");
-			Console.WriteLine("\nresult = {0}, Length = {1}, Capacity = {2}",
-				result, result.Length, result.Capacity);
+    public class StringBuilderLengthCapacity
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Stringresultfer length and capacity Methods\n");
 
-			result.EnsureCapacity( 10 );
-			Console.WriteLine("\nNew capacity = " + result.Capacity);
+            // New Stringbuilder string with "Hello"
+            StringBuilder result =
+                new StringBuilder("Hello");
 
-			result.EnsureCapacity( 33 );
-			Console.WriteLine("\nNew capacity = " + result.Capacity);
+            // Prints out the string, string length, string capacity, and its max capacity
+            Console.WriteLine("result = {0}, Length = {1}, Capacity = {2}, MaxCapacity = {3}",
+                             result, result.Length, result.Capacity, result.MaxCapacity);
 
-			result.Capacity = 60;
-			Console.WriteLine("\nNew capacity = " + result.Capacity);
+            // Add to the end of the string ", how are you?"
+            result.Append(", how are you?");
+            Console.WriteLine("\nresult = {0}, Length = {1}, Capacity = {2}",
+                result, result.Length, result.Capacity);
+            
+            // Capacity stays the same because it is already greater than 10
+            Console.WriteLine("\nNew capacity = " + result.EnsureCapacity(10));
 
-			Console.WriteLine();						
-		}
-	}
+            // The new capacity is 33 because it is greater than 32
+            Console.WriteLine("\nNew capacity = " + result.EnsureCapacity(33));
+
+            // Changes the capacity of result
+            result.Capacity = 60;
+            Console.WriteLine("\nNew capacity = " + result.Capacity);
+
+            Console.WriteLine();
+        }
+    }
 
 }
