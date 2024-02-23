4a361bc4cdf12408699b1dffe9bc75a38cff332a
Alexander Luna <59184968+alexanderfluna@users.noreply.github.com>
2024-02-23 11:37:25 -0500

    Update Coin.cs

--- Code/cs/basics_oop/cs299/4_Arrays/OldClassWorkPurse/Coin.cs
+++ Code/cs/basics_oop/cs299/4_Arrays/OldClassWorkPurse/Coin.cs
@@ -3,49 +3,52 @@ using System.Collections;
 
 namespace TestingProject
 {
-	/// <summary>
-	/// A coin with a monetary value.
-	/// </summary>
-	public class Coin	{
+    /// <summary>
+    /// A coin with a monetary value.
+    /// </summary>
+    public class Coin
+    {
 
-		///	Constructs a coin.
-		///	@param aValue the monetary value of the coin
-		///	@param aName the name of the coin
-		public Coin(double aValue, String aName) 
-		{ 
-			value = aValue; 
-			name = aName;
-		}
+        ///	Constructs a coin.
+        ///	@param aValue the monetary value of the coin
+        ///	@param aName the name of the coin
+        public Coin(double aValue, String aName)
+        {
+            value = aValue;
+            name = aName;
+        }
 
-		/// Gets the coin value.
-		/// @return the value
-		public double GetValue() 	
-		{
-			return value;
-		}
+        /// Gets the coin value.
+        /// @return the value
+        public double GetValue()
+        {
+            return value;
+        }
 
-		///	Gets the coin name.
-		/// @return the name
-		public String GetName() 
-		{
-			return name;
-		}
-		
-		public override bool Equals(Object otherObject)
-		{
-			Coin other = (Coin)otherObject;
-			return name==other.name
-				&& value == other.value;
-		}
+        ///	Gets the coin name.
+        /// @return the name
+        public String GetName()
+        {
+            return name;
+        }
 
-		// C# requirement: 
-		// since we override Equals, MUST also override GetHashCode ( !! )
-		public override int GetHashCode()
-		{
-			return base.GetHashCode ();
-		}		
+        // Override built-in C# Equals function
+        public override bool Equals(Object otherObject)
+        {
+            Coin other = (Coin)otherObject; // get the coin
+            return name == other.name // return true if the names and values of the fcoins match
+                && value == other.value;
+        }
 
-		private double value;
-		private string name;
-	}
+        // C# requirement: 
+        // since we override Equals, MUST also override GetHashCode ( !! )
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        // Private fields
+        private double value;
+        private string name;
+    }
 }
