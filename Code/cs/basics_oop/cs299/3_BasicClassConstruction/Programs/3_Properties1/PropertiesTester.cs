72497c53da95143f6ed7dfd85e1ab81859a7fc8b
Alexander Luna <59184968+alexanderfluna@users.noreply.github.com>
2024-02-23 11:10:47 -0500

    Update PropertiesTester.cs

--- Code/cs/basics_oop/cs299/3_BasicClassConstruction/Programs/3_Properties1/PropertiesTester.cs
+++ Code/cs/basics_oop/cs299/3_BasicClassConstruction/Programs/3_Properties1/PropertiesTester.cs
@@ -1,20 +1,30 @@
-﻿#region Using directives
+#region Using directives
 
 using System;
 using System.Text;
 
 #endregion
 
-namespace IntroducingProperties {
-  class PropertiesTester {
-	public static void Main() {
-		SimplePropertiesApplication app = new SimplePropertiesApplication(17);
-		app.ImportantValue2 = 14;
-		app.ImportantValue3 = "ABCDEFG";
-		Console.WriteLine("importantValue1 = " + app.ImportantValue1);
-		Console.WriteLine(app.ToString());
-		Console.ReadLine();
-	}
-  }
-}
+namespace IntroducingProperties
+{
+    class PropertiesTester
+    {
+        public static void Main()
+        {
+            // New app object
+            // importantvalue1 = 17
+            SimplePropertiesApplication app = new SimplePropertiesApplication(17);
 
+            // Assign important values
+            app.ImportantValue2 = 14;
+            app.ImportantValue3 = "ABCDEFG";
+
+            // print 17
+            Console.WriteLine("importantValue1 = " + app.ImportantValue1);
+
+            // print out three important values
+            Console.WriteLine(app.ToString());
+            Console.ReadLine();
+        }
+    }
+}
