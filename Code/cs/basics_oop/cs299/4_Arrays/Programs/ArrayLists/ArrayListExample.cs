fa7bbe31efc042384b7ed49536924659f5b5526f
Alexander Luna <59184968+alexanderfluna@users.noreply.github.com>
2024-02-23 11:44:18 -0500

    Update ArrayListExample.cs

--- Code/cs/basics_oop/cs299/4_Arrays/Programs/ArrayLists/ArrayListExample.cs
+++ Code/cs/basics_oop/cs299/4_Arrays/Programs/ArrayLists/ArrayListExample.cs
@@ -6,28 +6,43 @@
 
 using System;
 using System.Collections;
-public class ArrayListExample  
+public class ArrayListExample
 {
-    public static void Main( )  
+    public static void Main()
     {
+        // Create new array list
         ArrayList anArray = new ArrayList();
 
+        /* Alternate way to instantiate the array list:
+         * {
+            "Today is the first day of the rest of your life!",
+            "Live it to the fullest!",
+            "ok",
+            "You may not get a second chance."
+        };*/
+
+        // Add strings to the array list
         anArray.Add("Today is the first day of the rest of your life!");
         anArray.Add("Live it to the fullest!");
         anArray.Add("ok");
         anArray.Add("You may not get a second chance.");
 
-        Console.WriteLine("Count of elements in array:    {0}", anArray.Count );
-        anArray.RemoveAt(2);
-        Console.WriteLine("New Count (after removing ok): {0}", anArray.Count );
-        Console.WriteLine( );
-        DisplayContents(anArray );
+        // Print out size of array
+        Console.WriteLine("Count of elements in array:    {0}", anArray.Count);
+
+        anArray.RemoveAt(2); // remove element at index 2
+
+        // Print out size of array
+        Console.WriteLine("New Count (after removing ok): {0}", anArray.Count);
+        Console.WriteLine();
+        DisplayContents(anArray);
     }
 
-    public static void DisplayContents(ArrayList ar)  
+    // Print out contents of array
+    public static void DisplayContents(ArrayList ar)
     {
-       for(int i = 0; i < ar.Count; i ++)
-            Console.WriteLine(ar[i] );
+        for (int i = 0; i < ar.Count; i++)
+            Console.WriteLine(ar[i]);
         Console.WriteLine();
     }
 }
