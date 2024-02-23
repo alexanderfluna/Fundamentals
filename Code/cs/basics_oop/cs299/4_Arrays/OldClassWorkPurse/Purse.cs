5c8ab1f710395e5e90225a9158d58f501f3f880a
Alexander Luna <59184968+alexanderfluna@users.noreply.github.com>
2024-02-23 11:37:15 -0500

    Update Purse.cs

--- Code/cs/basics_oop/cs299/4_Arrays/OldClassWorkPurse/Purse.cs
+++ Code/cs/basics_oop/cs299/4_Arrays/OldClassWorkPurse/Purse.cs
@@ -3,45 +3,47 @@ using System.Collections;
 
 namespace TestingProject
 {
-	/// <summary>
-	/// A purse holds a collection of coins.
-	/// </summary>
-	public class Purse
-	{
-		///  Constructs an empty purse.
-		public Purse()
-		{
-			coins = new ArrayList();
-		}
+    /// <summary>
+    /// A purse holds a collection of coins.
+    /// </summary>
+    public class Purse
+    {
+        ///  Constructs an empty purse.
+        public Purse()
+        {
+            coins = new ArrayList();
+        }
 
-		///   Add a coin to the purse.
-		///   @param aCoin the coin to add
-		public void Add(Coin aCoin)
-		{
-			coins.Add(aCoin);
-		}
+        ///   Add a coin to the purse.
+        ///   @param aCoin the coin to add
+        public void Add(Coin aCoin)
+        {
+            coins.Add(aCoin); // adds an object to the end of an array list
+        }
 
-		///  Get the total value of the coins in the purse.
-		///  @return the sum of all coin values
-		public double GetTotal()
-		{
-			double total = 0;
-			for (int i = 0; i < coins.Count; i++)
-			{
-				Coin aCoin = (Coin)coins[i];
-				total = total + aCoin.GetValue();
-			}
-			return total;
-		}
+        ///  Get the total value of the coins in the purse.
+        ///  @return the sum of all coin values
+        public double GetTotal()
+        {
+            // Iterate through coins array list
+            double total = 0;
+            for (int i = 0; i < coins.Count; i++) // .Count returns the size of the array list
+            {
+                Coin aCoin = (Coin)coins[i]; // get the coin
+                total += aCoin.GetValue(); // add the coin's value to total
+            }
+            return total;
+        }
 
         public override string ToString()
         {
+            // Print out the name of each coin in the array list
             String result = "Purse[";
             for (int i = 0; i < coins.Count; i++)
             {
-                result = result + ((Coin) coins[i]).GetName();
+                result += ((Coin)coins[i]).GetName();
                 if (i < coins.Count - 1)
-                    result = result + ", ";
+                    result += ", ";
             }
             result += "]";
 
@@ -50,12 +52,12 @@ namespace TestingProject
 
         public void Reverse()
         {
-            coins.Reverse();
+            coins.Reverse(); // reverses the order of an array list
         }
-       
 
-		private ArrayList coins;
-	}
+
+        private ArrayList coins;
+    }
 
 
 
