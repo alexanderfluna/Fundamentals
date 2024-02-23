971131f1da5126438dbd995fe45c98de68ffbf14
Alexander Luna <59184968+alexanderfluna@users.noreply.github.com>
2024-02-23 11:21:24 -0500

    Add files via upload

---
+++ Code/cs/basics_oop/cs299/Programs/Arrays/PassingArray.cs
@@ -0,0 +1,38 @@
+/* PassingArray.cs      Author:  Doyle
+ * Demonstrates passing
+ * arrays to methods - references
+ */
+
+using System;
+namespace PassingArray
+{
+    class PassingArray
+    {
+        static void Main( )
+        {
+            int [ ] temperature = new int[5];
+            int [ ] t = new int[5];
+
+            InputValues(temperature);
+            
+            //Array t will reference the same array 
+            //as the temperature array
+            t = temperature;
+
+            t[1] = 44;
+            Console.WriteLine(temperature[1]);
+			
+        }
+        public static void InputValues(int [ ] temp)
+        {
+            string inValue;
+            
+            for(int i = 0; i < temp.Length; i ++)
+            {
+                Console.Write("Enter Temperature {0}: ", i + 1);
+                inValue = Console.ReadLine( );
+                temp[i] = int.Parse(inValue);
+            }
+        }
+    }
+}
