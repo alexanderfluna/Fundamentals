b8e1ec48f65257918ebf563508513e19b1f4428a
Alexander Luna <59184968+alexanderfluna@users.noreply.github.com>
2024-02-23 11:14:55 -0500

    Update CarCollectionTester.cs

--- Code/cs/basics_oop/cs299/3_BasicClassConstruction/Programs/6_Indexers/CarCollectionTester.cs
+++ Code/cs/basics_oop/cs299/3_BasicClassConstruction/Programs/6_Indexers/CarCollectionTester.cs
@@ -1,17 +1,29 @@
-﻿using System;
+using System;
 
-namespace IndexerApplication {
+namespace IndexerApplication
+{
 
-    public class CarCollectionApp {
+    public class CarCollectionApp
+    {
 
-        public static void Main() {
+        public static void Main()
+        {
+            // create car collection
             CarCollection myCars = new CarCollection();
-            for (int index = 1; index <= myCars.Size; index++) {
+
+            // print out every car
+            for (int index = 1; index <= myCars.Size; index++)
+            {
                 Console.WriteLine(myCars[index]);
             }
             Console.WriteLine();
+
+            // set car at index 2 to audi
             myCars[2] = "Audi";
-            for (int index = 1; index <= myCars.Size; index++) {
+
+            // print out every car
+            for (int index = 1; index <= myCars.Size; index++)
+            {
                 Console.WriteLine(myCars[index]);
             }
             Console.ReadLine();
