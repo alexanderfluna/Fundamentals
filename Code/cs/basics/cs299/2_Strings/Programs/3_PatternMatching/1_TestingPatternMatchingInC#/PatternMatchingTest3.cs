f73bb54e4e732634c9e2eb4b2ea7acd8554fa266
Alexander Luna <alexanderfluna@outlook.com>
2024-03-16 10:21:57 -0400

    Update

--- Code/cs/basics/cs299/2_Strings/Programs/3_PatternMatching/1_TestingPatternMatchingInC#/PatternMatchingTest3.cs
+++ Code/cs/basics/cs299/2_Strings/Programs/3_PatternMatching/1_TestingPatternMatchingInC#/PatternMatchingTest3.cs
@@ -1,41 +1,32 @@
 using System;
 using System.Text.RegularExpressions;
 
-namespace RegexpTest
-{  
-
-    class Program
+namespace Test {
+    public class Solution
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Match m;
-            
-            string text = "ABRACADABRA1abracadabra2abracadabra3";
-            string pat = @"abra";
-            Regex r = new Regex(pat, System.Text.RegularExpressions.RegexOptions.IgnoreCase);
-            m = r.Match(text);
-            Console.WriteLine("1 Match=" + m.ToString());
+            // Ignore case of the pattern
+            Match m = Regex.Match("ABRAcadABRA", "abra", RegexOptions.IgnoreCase);
+            if (m.Success)
+                Console.WriteLine("Input1: "+ m.Value);
 
-            pat = @"ab ra";
-            r = new Regex(pat,
-                System.Text.RegularExpressions.RegexOptions.IgnorePatternWhitespace);            
-            m = r.Match(text);
-            Console.WriteLine("2 Match=" + m.ToString());
-           
-            
-            text = "ABRACADABRA1\nabracadabra2\nabracadabra3";
-            pat = @"A1$";
-            m = r.Match(text);
-            Console.WriteLine("3 Match=" + m.ToString());
+            // Ignore white spaces in the pattern
+            m = Regex.Match("abra", "ab ra", RegexOptions.IgnorePatternWhitespace);
+            if (m.Success)
+                Console.WriteLine("Input2: " + m.Value);
 
-            r = new Regex(pat,
-                System.Text.RegularExpressions.RegexOptions.Multiline);
-            m = r.Match(text);
-            Console.WriteLine("4 Match=" + m.ToString());
+            // Does not work because it is not at the end of the line
+            m = Regex.Match("ABRACADABRA1\nabracadabra2\nabracadabra3", @"A1$");
+            if (m.Success)
+                Console.WriteLine("Input3: " + m.Value);
 
-            Console.ReadKey();
+            // This does work because of the multiline option
+            Regex r = new Regex(@"A1$",
+                RegexOptions.Multiline);
+            m = r.Match("ABRACADABRA1\nabracadabra2\nabracadabra3");
+            if (m.Success)
+                Console.WriteLine("Input4: " + m.Value);
         }
     }
-
-
 }
