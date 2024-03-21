efb00e4b28d77517dbe58893cd6fdab2af8651de
Alexander Luna <alexanderfluna@outlook.com>
2024-03-21 09:23:54 -0400

    Unsafe

--- Code/cs/basics/cs299/1_Overview/Programs/7_UnsafeCode/Unsafe.cs
+++ Code/cs/basics/cs299/1_Overview/Programs/7_UnsafeCode/Unsafe.cs
@@ -13,10 +13,19 @@ namespace __CsharpConsoleProject
             int var1 = 5;
             unsafe
             {
+                Console.WriteLine("var1 = " + var1);
+                Console.WriteLine("&var1 = " + (int)&var1);
+                
                 int* ptr1, ptr2;
+
                 ptr1 = &var1;
+                Console.WriteLine("ptr1 = " + (int)ptr1);
+                Console.WriteLine("*ptr1 = " + (int)*ptr1);
+
                 ptr2 = ptr1;
                 *ptr2 = 20;
+                Console.WriteLine("ptr2 = " + (int)ptr2);
+                Console.WriteLine("*ptr2 = " + (int)*ptr2);
 
             }
             Console.WriteLine(var1);
