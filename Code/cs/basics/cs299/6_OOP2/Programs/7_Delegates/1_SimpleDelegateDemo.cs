05520023572ac28f4b0bbbb05f1c07d7cecd72ea
Alexander Luna <59184968+alexanderfluna@users.noreply.github.com>
2024-03-26 11:22:09 -0400

    Update 1_SimpleDelegateDemo.cs

--- Code/cs/basics/cs299/6_OOP2/Programs/7_Delegates/1_SimpleDelegateDemo.cs
+++ Code/cs/basics/cs299/6_OOP2/Programs/7_Delegates/1_SimpleDelegateDemo.cs
@@ -1,57 +1,57 @@
-// SimpleDlegateDemo.cs - The first program using delegates
-
 using System;
 
-namespace ConsoleApplication1
+namespace VS_CS
 {
-	// 1. Define delegate
-	public delegate double UnitConversion(double from);
+    public delegate double UnitConversion(double from);
 
-	class QuickDelegateDemo
-	{
-		// 2. Define handler methods
-		public static double FeetToInches(double feet)
-		{
-			return feet * 12;
-		}
+    class DelegateDemo
+    {
+        
+        public static double FeetToInches(double feet)
+        {
+            return feet * 12;
+        }
+
         public static double InchesToFeet(double inches)
         {
             return inches / 12;
         }
-        // a method that uses a delegate as an argument
-        public static void printConversion (double num, 
-                                        UnitConversion conv){
+
+        public static void PrintConversion(double num, UnitConversion conv)
+        {
             double result = conv(num);
-            Console.WriteLine(num + " was converted to " 
-                                 + result);
+            Console.WriteLine("Num is: " + num);
         }
 
-		static void Main()
-		{
-			// Create delegate instance
-			UnitConversion DoConversion = new UnitConversion(FeetToInches);
+        static void Main(string[] args)
+        {
+            // Create a UnitConversion that uses the FeetToInches method
+            UnitConversion DoConversion = new UnitConversion(FeetToInches);
 
-			Console.Write("Please enter feet:  ");
-			double feet = Double.Parse(Console.ReadLine());
+            // Ask user for feet and store in feet var
+            Console.Write("Please enter feet: ");
+            double feet = Double.Parse(Console.ReadLine());
 
-			// Use delegate just like a method.
-			double inches = DoConversion(feet);
-			Console.WriteLine("{0} Feet = {1} Inches.", feet, inches);
-			
-            // Create delegate instance
+            // Convert from feet to inches using delegate
+            double inches = DoConversion(feet);
+            Console.WriteLine("{0} Feet = {1} Inches.", feet, inches);
+
+            // Change UnitConversion to use InchesToFeet method
             DoConversion = new UnitConversion(InchesToFeet);
-            Console.Write("Please enter inches:  ");
+            Console.Write("Please enter inches: ");
             inches = Double.Parse(Console.ReadLine());
 
-            // Use delegate just like a method.
+            // Convert from inches to feet using delegate
             feet = DoConversion(inches);
             Console.WriteLine("{0} Feet = {1} Inches.", feet, inches);
-                   
-            printConversion (inches, DoConversion);
-            DoConversion = new UnitConversion(FeetToInches);
-            printConversion(feet, DoConversion);
 
-            
-		}
-	}
+            // PrintConversion to print inches to feet
+            PrintConversion(inches, DoConversion);
+
+            // PrintConversion to print feet to inches
+            DoConversion = new UnitConversion(FeetToInches);
+            PrintConversion(feet, DoConversion);
+        }
+
+    }
 }
